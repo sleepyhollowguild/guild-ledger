3528a5074627594f22ae312f080547a0d20daa3a
Sleepy Hollow Guild <xerrik13@gmail.com>
2026-07-05 15:39:37 -0400

    Delete assets directory

--- assets/js/firebaseApp.js
+++
@@ -1,5 +0,0 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js';
-import { getDatabase } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js';
-import { firebaseConfig } from '../../firebase/config.js';
-export const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
